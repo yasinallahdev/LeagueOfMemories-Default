a171b289d19c668067be6d626dfd02f632beefbd
Voinea Andrei <andrei_voinea33@yahoo.com>
2018-04-23 00:49:05 +0300

    Changed the way Recall is handeled
    More details in the indev branch on the GameServer. Basically Recall is handled inside the Champion Class now

--- Champions/Global/Recall.cs
+++ Champions/Global/Recall.cs
@@ -13,13 +13,8 @@ namespace Spells
 
         public void OnFinishCasting(Champion owner, Spell spell, AttackableUnit target)
         {
-            var visualBuff = ApiFunctionManager.AddBuffHUDVisual("Recall", 8.0f, 1, owner);
-            var addParticle = ApiFunctionManager.AddParticleTarget(owner, "TeleportHome.troy", owner);
-            ApiFunctionManager.CreateTimer(8.0f, () =>
-            {
-                ApiFunctionManager.RemoveBuffHUDVisual(visualBuff);
-                owner.Recall(owner);
-            });
+            owner.canRecall = true;
+            owner.Recall(owner, 8.0f);
         }
 
         public void ApplyEffects(Champion owner, AttackableUnit target, Spell spell, Projectile projectile)
