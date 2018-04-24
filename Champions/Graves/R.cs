880175a1451b6b8c4502715c2bcd7ad8590920b6
Pokemonred200 <vegetafan201@gmail.com>
2018-04-24 16:09:52 -0400

    Added Miss Fortune's passive and accompanying buff. (#3)
    * Added Miss Fortune's passive and accompanying buff.
    
    Added Miss Fortune's Passive and Accompanying Buff. However, it will not load correctly at first push, as Miss Fortune's .json file (located in Stats/MissFortune) appears to be loading 'Draw a Bead' as her Passive, which Belongs to Tristana.
    
    * Added Graves's Q and R. Graves's E now properly grants Attack Speed.
    
    Graves's E, Quickdraw, what not giving the attack speed buff (30/40/50/60/80%) on activation, and the visuals were not disappearing from his screen. While working on fixing this, I have additionally implemented Buckshot and Collateral Damage.

--- Champions/Graves/R.cs
+++ Champions/Graves/R.cs
@@ -1,15 +1,15 @@
-using System.Numerics;
+﻿using System.Numerics;
 using LeagueSandbox.GameServer.Logic.GameObjects;
-using LeagueSandbox.GameServer.Logic.API;
 using LeagueSandbox.GameServer.Logic.GameObjects.AttackableUnits;
 using LeagueSandbox.GameServer.Logic.Scripting.CSharp;
 
 namespace Spells
 {
-    public class GravesMove : GameScript
+    public class GravesChargeShot : GameScript
     {
         public void OnActivate(Champion owner)
         {
+
         }
 
         public void OnDeactivate(Champion owner)
@@ -18,29 +18,25 @@ namespace Spells
 
         public void OnStartCasting(Champion owner, Spell spell, AttackableUnit target)
         {
+            spell.spellAnimation("SPELL4", owner);
         }
 
         public void OnFinishCasting(Champion owner, Spell spell, AttackableUnit target)
         {
             var current = new Vector2(owner.X, owner.Y);
             var to = Vector2.Normalize(new Vector2(spell.X, spell.Y) - current);
-            var range = to * 425;
+            var range = to * 1000;
             var trueCoords = current + range;
 
-            ApiFunctionManager.DashToLocation(owner, trueCoords.X, trueCoords.Y, 1200, false, "Spell3");
-            Particle p = ApiFunctionManager.AddParticleTarget(owner, "Graves_Move_OnBuffActivate.troy", owner);
-            var visualBuff = ApiFunctionManager.AddBuffHUDVisual("GravesSteroid", 5.0f, 90, owner);
-            var buff = owner.AddBuffGameScript("Quickdraw", "Quickdraw", spell, -1, true);
-            ApiFunctionManager.CreateTimer(4.0f, () =>
-            {
-                ApiFunctionManager.RemoveParticle(p);
-                ApiFunctionManager.RemoveBuffHUDVisual(visualBuff);
-                owner.RemoveBuffGameScript(buff);
-            });
+            spell.AddProjectile("GravesChargeShotShot", trueCoords.X, trueCoords.Y);
         }
 
         public void ApplyEffects(Champion owner, AttackableUnit target, Spell spell, Projectile projectile)
         {
+            var bonusAD = owner.GetStats().AttackDamage.Total - owner.GetStats().AttackDamage.BaseValue;
+            var ad = bonusAD * 0.150f;
+            var damage = new[] { 250, 400, 550 }[spell.Level - 1] + ad;
+            target.TakeDamage(owner, damage, DamageType.DAMAGE_TYPE_PHYSICAL, DamageSource.DAMAGE_SOURCE_SPELL, false);
         }
 
         public void OnUpdate(double diff)
