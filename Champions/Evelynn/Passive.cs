a931d3508c16541f4fe8cf7e3ed991a35535387d
_Bloop <justinschrier@gmail.com>
2018-05-19 06:15:11 -0700

    Evelynn first commit (#7)
    * Add files via upload
    
    Evelynn Passive, W and R all functional to their maximum with the current tech avalible.
    
    * Evelynn's Buffs
    
    EveInvis: Passive mana regeneration buff and will have invisibility when that can be added
    EveFrenzy: W active movespeed
    EveSpeed: Currently unused until I find how to add stacks to a buff up to a cap.
    
    * Delete Q.cs
    
    Q wasn't functional. Didn't mean to commit it.
    
    * Fixed R particle, moved W buff timer to where it should be.

--- Champions/Evelynn/Passive.cs
+++ Champions/Evelynn/Passive.cs
@@ -7,18 +7,43 @@ namespace Spells
 {
     public class EvelynnPassive : GameScript
     {
+
+        private double _lastTakenDamage;
+        private double _currentTime;
+        private bool _invisActive;
+        private Champion _owningChampion;
+        private Spell _owningSpell;
+        private BuffGameScriptController InvisBuff;
+
+        private bool _frenzyLearned;
+        private BuffGameScriptController FrenzyBuff;
+        private Spell _frenzy;
+
         public void OnActivate(Champion owner)
         {
+            _lastTakenDamage = 0;
+            _currentTime = 0;
+            _owningChampion = owner;
+            _invisActive = false;
             ApiEventManager.OnChampionDamageTaken.AddListener(this, owner, SelfWasDamaged);
+
+            _frenzyLearned = false;
+            _frenzy = _owningChampion.GetSpell(1);
         }
 
         private void SelfWasDamaged()
         {
+            _lastTakenDamage = _currentTime;
+            if (_invisActive && (InvisBuff != null))
+            {
+                _owningChampion.RemoveBuffGameScript(InvisBuff);
+            }
+            _invisActive = false;
+            ApiFunctionManager.LogInfo("Evelynn took damage. Invisibility removed.");
         }
 
         public void OnDeactivate(Champion owner)
         {
-            //Listeners are automatically removed when GameScripts deactivate
         }
 
         public void OnStartCasting(Champion owner, Spell spell, AttackableUnit target)
@@ -35,7 +60,48 @@ namespace Spells
 
         public void OnUpdate(double diff)
         {
+            //TODO:Enable this when a way to give speed stacks is found
+            /*if (!_frenzyLearned)
+            {
+                if (_frenzy.Level >= 1)
+                {
+                    _frenzyLearned = true;
+                }
+            }
+
+            if (_frenzyLearned)
+            {
+                FrenzyBuff = _owningChampion.AddBuffGameScript("EveSpeed", "EveSpeed", _frenzy, -1, true);
+            }*/
+
+            if (_owningChampion == null)
+            {
+                return;
+            }
+
+            if (_owningSpell == null)
+            {
+                _owningSpell = _owningChampion.GetSpell(14);
+                if (_owningSpell == null)
+                {
+                    return;
+                }
+            }
+
+            _currentTime += diff;
+            if (_currentTime >= (_lastTakenDamage + 6000))
+            {
+                if (!_invisActive)
+                {
+                    ApiFunctionManager.LogInfo("Evelynn hasn't been damaged in the last 6 second. Shadow Walk activated.");
+                    InvisBuff = _owningChampion.AddBuffGameScript("EveInvis", "EveInvis", _owningSpell, -1, true);
+                    _invisActive = true;
+                }
+                else
+                {
+                    InvisBuff.UpdateBuff(diff);
+                }
+            }
         }
     }
 }
-
