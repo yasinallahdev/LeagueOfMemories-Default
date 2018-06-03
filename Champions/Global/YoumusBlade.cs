b85b42289dcfafca07dd5e03bb865040fa0bb9c9
Pokemonred200 <vegetafan201@gmail.com>
2018-06-03 18:38:50 -0400

    Updated the functionality of several summoner spells, and added some item actives.
    Ignite now properly applies Grievous Wounds, while Heal is now effected by it. Smite now uses the same level scaling League of Legends officially used in 4.20. Additionally, the items (and buffs accompanying them) Talisman of Ascension, Mana Potion, Health Potion, Youmuu's Ghostblade, Tiamat, Ravenous Hydra, and Talisman of Ascension (the final of which is referenced by its original name, Shurelya's Reverie) have their active componets implemented here. Finally, the passive effect of Blade of the Ruined King is implemented as well.

--- Champions/Global/YoumusBlade.cs
+++ Champions/Global/YoumusBlade.cs
@@ -1,17 +1,20 @@
-using LeagueSandbox.GameServer.Logic.GameObjects;
+﻿using LeagueSandbox.GameServer.Logic.GameObjects;
 using LeagueSandbox.GameServer.Logic.API;
 using LeagueSandbox.GameServer.Logic.Scripting.CSharp;
 using LeagueSandbox.GameServer.Logic.GameObjects.AttackableUnits;
 
 namespace Spells
 {
-    public class SummonerSmite : GameScript
+    public class YoumusBlade : GameScript
     {
         public void OnStartCasting(Champion owner, Spell spell, AttackableUnit target)
         {
-            ApiFunctionManager.AddParticleTarget(owner, "Global_SS_Smite.troy", target, 1);
-            var damage = (new float[] { 390, 410, 430, 450, 480, 510, 540, 570, 600, 640, 680, 720, 760, 800, 850, 900, 950, 1000}) [owner.GetStats().Level - 1];
-            target.TakeDamage(owner, damage, DamageType.DAMAGE_TYPE_TRUE, DamageSource.DAMAGE_SOURCE_SUMMONER_SPELL, false);
+            var buff = ((ObjAIBase)target).AddBuffGameScript("YoumusBlade", "YoumusBlade", spell,6.0f);
+            var p = ApiFunctionManager.AddParticleTarget(owner, "spectral_fury_activate_speed.troy", owner, 2);
+            ApiFunctionManager.CreateTimer(6.0f, () =>
+             {
+                 ApiFunctionManager.RemoveParticle(p);
+             });
         }
 
         public void OnFinishCasting(Champion owner, Spell spell, AttackableUnit target)
@@ -35,4 +38,3 @@ namespace Spells
         }
     }
 }
-
