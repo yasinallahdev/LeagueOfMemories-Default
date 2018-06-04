4ff2b57bd989932b3a9974a4784d4d7fffdbe1c4
Pokemonred200 <vegetafan201@gmail.com>
2018-06-04 02:22:46 -0400

    Updated the functionality of several summoner spells, and added some item actives. (#15)
    Ignite now properly applies Grievous Wounds, while Heal is now effected by it. Smite now uses the same level scaling League of Legends officially used in 4.20. Additionally, the items (and buffs accompanying them) Talisman of Ascension, Mana Potion, Health Potion, Youmuu's Ghostblade, Tiamat, Ravenous Hydra, and Talisman of Ascension (the final of which is referenced by its original name, Shurelya's Reverie) have their active componets implemented here. Finally, the passive effect of Blade of the Ruined King is implemented as well.

--- Champions/Global/ItemSwordOfFeastAndFamine.cs
+++ Champions/Global/ItemSwordOfFeastAndFamine.cs
@@ -1,17 +1,18 @@
-using LeagueSandbox.GameServer.Logic.GameObjects;
+﻿using LeagueSandbox.GameServer.Logic.GameObjects;
 using LeagueSandbox.GameServer.Logic.API;
 using LeagueSandbox.GameServer.Logic.Scripting.CSharp;
 using LeagueSandbox.GameServer.Logic.GameObjects.AttackableUnits;
 
 namespace Spells
 {
-    public class SummonerSmite : GameScript
+    public class ItemSwordOfFeastAndFamine : GameScript
     {
+
+        private Champion _owningChampion;
+        private BuffGameScriptController _botrkBuff;
+
         public void OnStartCasting(Champion owner, Spell spell, AttackableUnit target)
         {
-            ApiFunctionManager.AddParticleTarget(owner, "Global_SS_Smite.troy", target, 1);
-            var damage = (new float[] { 390, 410, 430, 450, 480, 510, 540, 570, 600, 640, 680, 720, 760, 800, 850, 900, 950, 1000}) [owner.GetStats().Level - 1];
-            target.TakeDamage(owner, damage, DamageType.DAMAGE_TYPE_TRUE, DamageSource.DAMAGE_SOURCE_SUMMONER_SPELL, false);
         }
 
         public void OnFinishCasting(Champion owner, Spell spell, AttackableUnit target)
@@ -28,11 +29,13 @@ namespace Spells
 
         public void OnActivate(Champion owner)
         {
+            _owningChampion = owner;
+            _botrkBuff = owner.AddBuffGameScript("BotrkBuff", "BotrkBuff", null);
         }
 
         public void OnDeactivate(Champion owner)
         {
+            owner.RemoveBuffGameScript(_botrkBuff);
         }
     }
 }
-
--- Champions/Global/SummonerExhaust.cs
+++ Champions/Global/SummonerExhaust.cs
@@ -13,18 +13,23 @@ namespace Spells
 
         public void OnFinishCasting(Champion owner, Spell spell, AttackableUnit target)
         {
+            ObjAIBase aiTarget = target as ObjAIBase;
+            if(aiTarget == null)
+            {
+                return;
+            }
             ChampionStatModifier statMod = new ChampionStatModifier();
             statMod.MoveSpeed.PercentBonus -= 30.0f / 100.0f;
             statMod.AttackSpeed.PercentBonus -= 30.0f / 100.0f;
             statMod.Armor.BaseBonus -= 10;
             statMod.MagicResist.BaseBonus -= 10;
-            target.AddStatModifier(statMod);
+            aiTarget.AddStatModifier(statMod);
             ApiFunctionManager.AddParticleTarget(owner, "Global_SS_Exhaust.troy", target);
             var visualBuff = ApiFunctionManager.AddBuffHUDVisual("SummonerExhaustDebuff", 2.5f, 1, (ObjAIBase) target);
             ApiFunctionManager.CreateTimer(2.5f, () =>
             {
                 ApiFunctionManager.RemoveBuffHUDVisual(visualBuff);
-                target.RemoveStatModifier(statMod);
+                aiTarget.RemoveStatModifier(statMod);
             });
         }
 
